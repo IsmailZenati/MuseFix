aa7b3408f222302f2f6bd6d7f99a31bbeb515e93
mohamedkhalil126 <144931860+mohamedkhalil126@users.noreply.github.com>
2024-02-28 13:17:24 +0100

    seventh subscription commit

--- abonnement-pack/src/main/java/controllers/ModifierPackController.java
+++ abonnement-pack/src/main/java/controllers/ModifierPackController.java
@@ -1,4 +1,50 @@
 package controllers;
 
+
+import entities.packs;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import services.ServicePack;
+
+import java.sql.SQLException;
+
 public class ModifierPackController {
+
+    private final ServicePack servicePack = new ServicePack();
+
+    @FXML
+    private TextField tf_packId;
+
+    @FXML
+    void modifierPack(ActionEvent event) {
+        int packId = Integer.parseInt(tf_packId.getText());
+        try {
+            // Votre logique pour modifier le pack ici
+            servicePack.modifier(new packs());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setContentText("Pack modifié");
+            alert.showAndWait();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML
+    void supprimerPack(ActionEvent event) {
+        int packId = Integer.parseInt(tf_packId.getText());
+        try {
+            // Votre logique pour supprimer le pack ici
+            servicePack.supprimer(packId);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setContentText("Pack supprimé");
+            alert.showAndWait();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
--- abonnement-pack/src/main/java/tests/MainFX.java
+++ abonnement-pack/src/main/java/tests/MainFX.java
@@ -16,8 +16,8 @@ public class MainFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/AfficherAbonnement.fxml"));
-        FXMLLoader fxmlLoader2 = new FXMLLoader(getClass().getResource("/ModifierAbonnement.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/AfficherPack.fxml"));
+        FXMLLoader fxmlLoader2 = new FXMLLoader(getClass().getResource("/ModifierPack.fxml"));
         try {
             Parent root = fxmlLoader.load();
             Parent root2 = fxmlLoader2.load();
@@ -26,12 +26,12 @@ public class MainFX extends Application {
             Scene scene2 = new Scene(root2);
 
             primaryStage.setScene(scene);
-            primaryStage.setTitle("Afficher abonnements");
+            primaryStage.setTitle("Afficher les packs");
             primaryStage.show();
 
             Stage secondaryStage = new Stage();
             secondaryStage.setScene(scene2);
-            secondaryStage.setTitle("Modifier abonnements");
+            secondaryStage.setTitle("Modifier  les packs");
             secondaryStage.show();
 
         } catch (IOException e) {
